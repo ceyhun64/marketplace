196e446649f9af3a5de1a828fe76e8383fc1d083
Ceyhun Turkmen <ctrkmn64@gmail.com>
2026-04-29 14:18:45 +0300

    108. commit

--- web/app/merchant/layout.tsx
+++ web/app/merchant/layout.tsx
@@ -17,7 +17,7 @@ export default function MerchantLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen bg-gray-50">
+    <div className="flex min-h-screen">
       <Sidebar links={merchantLinks} role="Merchant" />
       <main className="flex-1 ml-60 p-8">{children}</main>
     </div>
